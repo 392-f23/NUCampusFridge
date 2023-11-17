de54c48675e0d55a419a2a69f3dca24abedfcaf2
Neel Keswani <neel@Neels-MacBook-Pro.local>
2023-11-16 20:52:03 -0600

    Pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { NextUIProvider } from "@nextui-org/react";
 import './index.css';
-import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import "leaflet/dist/leaflet.css";
+
+import ErrorPage from './pages/ErrorPage';
+import Root from './routes/root';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4E2A84'
+    },
+    darkbackground: {
+      main: '#fff',
+      contrastText: '#000'
+    },
+    contrast: {
+      main: '#6D28D9',
+      light: '#fff',
+      dark: '#4c1d95',
+      contrastText: '#fff'
+    },
+    purple: {
+      main: '#4e2a84',
+    },
+  }
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+
+    ],
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NextUIProvider>
+        <RouterProvider router={router} />
+      </NextUIProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/routes/root.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import "./root.css";
+import useDealsStore from "../utilities/stores";
+import { getDbData } from "../utilities/firebase";
+import { useLocation } from 'react-router-dom';
+import Banner from "../components/Banner";
+
+const Root = () => {
+    const setBusiness = useDealsStore((state) => state.setBusiness);
+    const business = useDealsStore((state) => state.business);
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+
+    useEffect(() => {
+        getDbData("/businesses").then((data) => {
+            setBusiness(data);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }, []);
+
+    return (
+        <div className="App min-h-screen flex flex-col">
+            {pathname !== '/login' &&
+                <Banner />
+            }
+            <div className="flex-grow">
+                {business ? (
+                    <Outlet />
+                ) : (
+                    <div className="text-center mt-8">
+                        <p className="text-lg font-bold text-white">Loading...Paws for a moment</p>
+                    </div>
+                )}
+            </div>
+            {pathname !== '/map' && pathname !== '/login' &&
+                <footer className="w-full p-8">
+                    <p className="text-center text-default-500 text-sm">Northwestern University</p>
+                    <p className="text-center text-default-500 text-sm">© {new Date().getFullYear()} Northwestern Campus Fridge</p>
+                </footer>
+            }
+        </div>
+    );
+};
+
+export default Root;
