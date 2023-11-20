ea662038ef1d6b119d05c87240725062d9571bec
Hunterxia <hunterxia@outlook.com>
2023-11-20 14:47:16 -0600

    Fix import statements and add logo URLs to main data

--- src/index.jsx
+++ src/index.jsx
@@ -1,37 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
-import './index.css';
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import "./index.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 
-import ErrorPage from './pages/ErrorPage';
-import Root from './routes/root';
-import ItemPage from './pages/ItemPage';
+import ErrorPage from "./pages/ErrorPage";
+import Root from "./routes/root";
+import ItemPage from "./pages/ItemPage";
+// import NewItemPage from "./pages/NewItemPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#4E2A84'
+      main: "#4E2A84",
     },
     darkbackground: {
-      main: '#fff',
-      contrastText: '#000'
+      main: "#fff",
+      contrastText: "#000",
     },
     contrast: {
-      main: '#6D28D9',
-      light: '#fff',
-      dark: '#4c1d95',
-      contrastText: '#fff'
+      main: "#6D28D9",
+      light: "#fff",
+      dark: "#4c1d95",
+      contrastText: "#fff",
     },
     purple: {
-      main: '#4e2a84',
+      main: "#4e2a84",
     },
-  }
+  },
 });
 
 const router = createBrowserRouter([
@@ -41,6 +42,10 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <ItemPage /> },
+      // {
+      //   path: "newitem",
+      //   element: <NewItemPage />,
+      // },
     ],
   },
 ]);
