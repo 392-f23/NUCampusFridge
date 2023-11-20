020c9e592d680e59dc1fe7410ca59d609b578351
Hunterxia <hunterxia@outlook.com>
2023-11-20 11:16:37 -0600

    Refactor CardModal component styling

--- src/components/CardModal.jsx
+++ src/components/CardModal.jsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400, // Consider making this responsive
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
@@ -23,7 +23,14 @@ const CardModal = ({ item }) => {
 
   return (
     <div>
-      <Button onClick={handleOpen} size="small">Learn More</Button>
+      <Button
+        variant="contained"
+        onClick={handleOpen}
+        size="small"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Learn More
+      </Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -31,14 +38,28 @@ const CardModal = ({ item }) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            className="text-center text-gray-900"
+          >
             {item.Category}
           </Typography>
-
+          <div className="mt-2 px-7 py-3">
+            <p className="text-sm text-gray-500">Location: {item.Location}</p>
+            <p className="text-sm text-gray-500">
+              {/* Weight: {item["Weight (in lbs)"]} lbs */}
+              Weight: 12 lbs
+            </p>
+            <p className="text-sm text-gray-500">
+              Arrival Temperature: 57.7 °F
+            </p>
+          </div>
         </Box>
       </Modal>
     </div>
   );
-}
+};
 
 export default CardModal;
