238703c1fca97d60ed90e5a4affb7498e8c1a0bb
Hunterxia <hunterxia@outlook.com>
2023-11-27 14:45:30 -0600

    Refactor ItemCard and ItemPage components

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import CardMedia from "@mui/material/CardMedia"; // Import CardMedia
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import CardModal from "./CardModal";
 
 export default function ItemCard({ item }) {
+  const defaultPictureUrl =
+    "https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg";
+  const imageUrl =
+    item.ImageURL && item.Item !== "..." ? item.ImageURL : defaultPictureUrl;
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardContent>
@@ -17,6 +23,12 @@ export default function ItemCard({ item }) {
           {item.Category}
         </Typography>
       </CardContent>
+      <CardMedia
+        component="img"
+        height="194"
+        image={imageUrl}
+        alt={item.Location || "Default Image"} // Fallback alt text if Location is not available
+      />
       <CardActions>
         <Button size="small">Share</Button>
         <CardModal item={item} />
--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,31 +1,32 @@
-
-import React, { useState, useEffect } from 'react';
-import Banner from '../components/Banner';
-import ItemsDisplay from '../components/ItemsDisplay';
+import React, { useState, useEffect } from "react";
+import Banner from "../components/Banner";
+import ItemsDisplay from "../components/ItemsDisplay";
 import { getDbData } from "../utilities/firebase";
 
 const ItemPage = () => {
-    const [items, setItems] = useState([]);
-    const [searchQuery, setSearchQuery] = useState('');
+  const [items, setItems] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
-    useEffect(() => {
-        getDbData("/Data").then((fetchedItems) => {
-            setItems(fetchedItems);
-        }).catch((error) => {
-            console.error("Error fetching items:", error);
-        });
-    }, []);
+  useEffect(() => {
+    getDbData("/Data")
+      .then((fetchedItems) => {
+        setItems(fetchedItems);
+      })
+      .catch((error) => {
+        console.error("Error fetching items:", error);
+      });
+  }, []);
 
-    if (items.length === 0) {
-        return <div>Loading items...</div>;
-    }
+  if (items.length === 0) {
+    return <div>Loading items...</div>;
+  }
 
-    return (
-        <div>
-            <Banner handleSearch={setSearchQuery} />
-            <ItemsDisplay items={items} searchQuery={searchQuery} />
-        </div>
-    );
+  return (
+    <div>
+      <Banner handleSearch={setSearchQuery} />
+      <ItemsDisplay items={items} searchQuery={searchQuery} />
+    </div>
+  );
 };
 
 export default ItemPage;
